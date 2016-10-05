90b9b9a7bf5818367c4477acec019cea90b6a9ae
Sawako Ishida <sawako.yoriko@gmail.com>
2016-10-05 15:23:10 -0700

    added incident point schema

--- app/routes.js
+++ app/routes.js
@@ -7,12 +7,12 @@ module.exports = function(app, morgan, socrata){
 		socrata.getDataSet()
 		.then((data)=>{
 			debug('inside of then');
+			console.log(data);
 			let parsedData = JSON.parse(data);
 			let coords = parsedData.map((obj)=>{
 				return {
 					event_clearance_group: obj.event_clearance_group,
-					latitude: obj.latitude,
-					longitude: obj.longitude,
+					incident_location: obj.incident_location
 				}
 			});
 			res.json({data: coords});

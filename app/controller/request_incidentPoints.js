cf114ac5fdd0c70b6fcc3e7a0504b154de63b8ee
Goutham Reddy <gouthamvreddy@gmail.com>
2016-10-17 23:56:16 -0700

    Refactored fetching data from Socrata and renamed file to request_incidentPoints. Deleted files get_data and index in app/controller folder.

---
+++ app/controller/request_incidentPoints.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const debug = require('debug')('seattle911:socrata_data');
+const Promise = require('bluebird');
+const config = require('../../config.js');
+const request = Promise.promisify(require('request'));
+
+module.exports = (startDate, endDate) => {
+	const dataQueryURL = `${config.socrataURL}?$where=event_clearance_date%20between%20"${startDate}"%20and%20"${endDate}"`;
+	debug('queryURL');
+	debug(dataQueryURL);
+	return request(dataQueryURL).then((res) => {
+		debug('socrata_data');
+		debug(res.statusCode);
+		return res.body;
+	});
+};

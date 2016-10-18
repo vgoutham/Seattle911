bf46e588211e390e814908b77dc2d6d33207cd0a
Goutham Reddy <gouthamvreddy@gmail.com>
2016-10-17 18:23:49 -0700

    Refactored server.js file and added test geojson file

--- server.js
+++ server.js
@@ -5,35 +5,33 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const config = require('./config.js');
 const mongoose = require('mongoose');
-const Socrata = require('./app/controller/socrata_data.js');
-const PointController = require('./app/controller/incident_points_ctrl.js');
-const AllocateData = require('./app/controller/sort_data');
 const Moment = require('moment');
-require('./app/routes.js')(app, morgan, Socrata, PointController);
 
-// mongoose.connect(config.db);
-app.use(cors({
-  'Accept-Encoding': ['gzip']
-}));
+const config = require('./config.js');
+const Socrata = require('./app/controller/socrata_data.js');
+
+mongoose.connect(config.db);
+
+//don't show the log when it is test
+if(config.util.getEnv('NODE_ENV') !== 'test') {
+  //use morgan to log at command line
+  app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
+
+app.use(
+  cors({'Accept-Encoding': ['gzip']})
+);
+
 app.use(bodyParser.json());
 
-//every 24 hours updates data
-  // TODO: use momentjs to change dates
-Socrata.getDataSet('2016-09-01T00:00:00','2016-09-30T23:59:00')
-.then((data)=>{
-  // debug(data);
-  PointController.addSuperGroup(data, AllocateData.superGroups, PointController.storeIncidentPoints);
-})
-.catch((err)=>{
-  debug(err);
-});
+//Update database with new data every hour
+const interval = 1000 * 60 * 60;
+setInterval(
+  function(){
+    //Update Db here
+  },
+  interval
+);
 
-// }, 100000);
-// getDataEveryDay.getNewSetOfData();
-
-
-const server = app.listen(config.port, () => {console.log(`Server is running on port: ${config.port}`);});
-server.isRunning = true;
-module.exports = server;
+app.listen(config.port, () => {console.log(`Server is running on port: ${config.port}`);});

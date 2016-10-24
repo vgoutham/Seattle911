3f0d2578c4696937eb337ad3754eaab673696de8
Sawako <sawako.yoriko@gmail.com>
2016-10-23 21:40:29 -0700

    moved baseurl out of config

--- app/controller/request_incidentPoints.js
+++ app/controller/request_incidentPoints.js
@@ -4,12 +4,11 @@ const debug = require('debug')('seattle911:socrata_data');
 const Promise = require('bluebird');
 const config = require('../../config.js');
 const request = Promise.promisify(require('request'));
-// const rp = require('request-promise');
+const baseUrl = 'https://data.seattle.gov';
 
 module.exports.getSocrataData = (startDate, endDate) => {
 	debug('hitting Socrata api');
-	const dataQueryURL = `${config.socrataURL}/resource/pu5n-trf4.geojson?$where=event_clearance_date%20between%20"${startDate}"%20and%20"${endDate}"`;
-		return request(dataQueryURL).then((res)=>{
+		return request(`${baseUrl}/resource/pu5n-trf4.geojson?$where=event_clearance_date%20between%20"${startDate}"%20and%20"${endDate}"`).then((res)=>{
 			debug('socrata_data');
 			debug(res.statusCode);
 			return JSON.parse(res.body);

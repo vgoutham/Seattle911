f3cfe4bab75bfc2fa9657662afa5ea5a8e8d849c
Sawako <sawako.yoriko@gmail.com>
2016-10-23 21:32:11 -0700

    removed property obj from incident_points_ctrl.js and option and newSetOfData

--- app/controller/incident_points_ctrl.js
+++ app/controller/incident_points_ctrl.js
@@ -2,51 +2,7 @@
 const IncidentPoint = require('../model/incident_point');
 const debug = require('debug')('seattle911:incident_points_ctrl');
 const _= require('lodash');
-let option;
-let newSetOfData = [];
-const supergroupProp = {
-  'TRESPASS': 'PROPERTY',
-  'SHOPLIFTING': 'PROPERTY',
-  'DISTURBANCES': 'MISC',
-  'BIKE': 'PROPERTY',
-  'OTHER PROPERTY': 'PROPERTY',
-  'CAR PROWL': 'PROPERTY',
-  'OTHER VICE': 'VICE',
-  'AUTO THEFTS': 'PROPERTY',
-  'TRAFFIC RELATED CALLS': 'VEHICLE',
-  'PERSON DOWN/INJURY': 'ASSIST',
-  'PROPERTY DAMAGE': 'PROPERTY',
-  'LIQUOR VIOLATIONS': 'VICE',
-  'SUSPICIOUS CIRCUMSTANCES': 'MISC',
-  'BEHAVIORAL HEALTH': 'MISC',
-  'LEWD CONDUCT': 'VICE',
-  'FRAUD CALLS': 'PROPERTY',
-  'ROBBERY': 'VIOLENT',
-  'ASSAULTS': 'VIOLENT',
-  'NUISANCE, MISCHIEF': 'MISC',
-  'PROPERTY - MISSING, FOUND': 'PROPERTY',
-  'MISC MISDEMEANORS': 'MISC',
-  'THREATS, HARASSMENT': 'VIOLENT',
-  'PERSONS - LOST, FOUND, MISSING': 'ASSIST',
-  'BURGLARY': 'PROPERTY',
-  'MOTOR VEHICLE COLLISION INVESTIGATION': 'VEHICLE',
-  'FALSE ALARMS': 'MISC',
-  'ARREST': 'MISC',
-  'WEAPONS CALLS': 'MISC',
-  'PROSTITUTION': 'VICE',
-  'HAZARDS': 'MISC',
-  'NARCOTICS COMPLAINTS': 'VICE',
-  'PROWLER': 'MISC',
-  'ANIMAL COMPLAINTS': 'MISC',
-  'RECKLESS BURNING': 'MISC',
-  'HARBOR CALLS': 'VEHICLE',
-  'PUBLIC GATHERINGS': 'MISC',
-  'DRIVE BY (NO INJURY)': 'VIOLENT',
-  'HOMICIDE': 'VIOLENT',
-  'FAILURE TO REGISTER (SEX OFFENDER)': 'MISC',
-  'FALSE ALACAD': 'MISC',
-  'VICE CALLS': 'VICE'
-};
+const supergroupProp = require('./reformat_data.js');
 
 
 //upsertingng data

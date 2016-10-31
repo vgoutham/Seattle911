44fb899f7d2e2c73b793bb3a7aaee66032aca440
Goutham Reddy <gouthamvreddy@gmail.com>
2016-10-30 19:18:32 -0700

    Updated config arrow function spacing and removed unnecessary code

--- config.db.js
+++ config.db.js
@@ -10,51 +10,41 @@ const Neighbourhood = require('./app/model/neighbourhood');
 const superGroups = require('./app/controller/reformat_data');
 const getIncidentPoints = require('./app/controller/request_incidentPoints');
 const IncidentPointCtr = require('./app/controller/incident_points_ctrl');
-const supergroupProp = require('./app/controller/reformat_data.js');
 const readdir = Promise.promisify(require('fs').readdir);
 const readfile = Promise.promisify(require('fs').readFile);
 
 mongoose.connect(config.db);
 
 IncidentPoint.collection.drop();
+Neighbourhood.collection.drop();
 
 const startDate = moment().subtract(1, 'month').format().slice(0, -6); //1 month prior to current datetime
 const endDate = moment().format().slice(0, -6); //current datetime
 
+//Get last month of incidents points from Socrata API
+getIncidentPoints.getSocrataData(startDate, endDate).then((response) => {
+  //then add _id and super groups to each incident
+  const incidents = IncidentPointCtr.addSuperGroup(response.data);
 
-  // let superGroupedData;
-  //Get last month of incidents points from Socrata API
-  getIncidentPoints.getSocrataData(startDate, endDate).then((response) => {
-    //then add _id and super groups to each incident
-    let superGroupedData = IncidentPointCtr.addSuperGroup(response.data, supergroupProp);
+  //Insert database with incidents
+  IncidentPoint.collection.insert(incidents, (err, res) => {
+    if(err) throw err;
+    console.log(`${res.insertedCount} incidents added to database!`);
+    mongoose.connection.close();
+  });
+});
 
-    //Insert database with incidents
-    IncidentPoint.collection.insert(superGroupedData, (err, res) => {
-      if(err) throw err;
-      console.log(`${res.insertedCount} incidents added to database!`);
-      mongoose.connection.close();
+readdir('neighborhoods').then((files) => {
+  files.forEach((file) => {
+    readfile(`./neighborhoods/${file}`).then((geojson) => {
+      const neighborhood = file.slice(0, -8);
+      const addAreaName = _.merge(JSON.parse(geojson.toString()), {properties: {area: neighborhood}});
+      const NeighbourhoodArea = new Neighbourhood(addAreaName);
+      NeighbourhoodArea.save();
     });
   });
-
-readdir('geojson').then((files)=>{
-  files.forEach((file)=>{
-    readfile(`./geojson/${file}`).then((geoObj)=>{
-      let str = file.slice(0, -8);
-      neighbourhoodGeo(geoObj).then((geo)=>{
-        let addAreaName = _.merge(JSON.parse(geo), {properties: {area: str }});
-        let NeighbourhoodArea = new Neighbourhood(addAreaName);
-        NeighbourhoodArea.save();
-      });
-    });
-  });//end of forEach
 })
-.catch((err)=>{
+.catch((err) => {
   debug(err);
   console.error(err);
-});//end of readdir
-
-let neighbourhoodGeo = module.exports = (geoObj)=>{
-  return new Promise((resolve, reject)=>{
-    resolve(geoObj.toString());
-  });
-};
+});
